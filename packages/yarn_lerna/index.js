a5dc667d181fd1b6fd74101134f50c8a9e21b457
Seyyed Mehdi Hassanpour <seyyedkhandon@gmail.com>
2020-06-24 16:36:39 +0430

    bugfix: fix duplicated package1 to package2

--- packages/yarn_lerna/index.js
+++ packages/yarn_lerna/index.js
@@ -1,5 +1,5 @@
 const commonFunction1 = require("@yarn_lerna/package1");
-const commonFunction2 = require("@yarn_lerna/package1");
+const commonFunction2 = require("@yarn_lerna/package2");
 
 module.exports= {
   commonFunction1,
